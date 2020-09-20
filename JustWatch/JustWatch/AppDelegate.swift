f8abb24ca031e2d74b4a722859ca4a0b6bee0aa0
SEO <sirm0su@gmail.com>
2020-09-20 21:59:53 +0900

    씬 델리게이트 하위호환 적용

--- JustWatch/JustWatch/AppDelegate.swift
+++ JustWatch/JustWatch/AppDelegate.swift
@@ -18,8 +18,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    
 
+
+}
+
+@available(iOS 13.0, *)
+// MARK: UISceneSession Lifecycle
+extension AppDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -31,7 +37,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
-
