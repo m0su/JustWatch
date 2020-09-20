fbaaadb725fa8cf352624287ba0b8ffcb56b40d0
SEO <sirm0su@gmail.com>
2020-09-20 21:58:26 +0900

    SwiftUI -> Storyboard로 변경

---
+++ JustWatch/JustWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JustWatch
+//
+//  Created by SEO on 2020/09/20.
+//  Copyright © 2020 SEO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
