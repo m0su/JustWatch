9acc6f3a35d33865aeba38b2e5cae51f55efe1d2
m0su <sirm0su@gmail.com>
2020-09-20 22:21:59 +0900

    HH:mm:ss 스타일로 시간 보기

--- JustWatch/JustWatch/ViewController.swift
+++ JustWatch/JustWatch/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import Then
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var timeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupTimer()
+    }
+    
+    func setupTimer() {
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+        
+        
+    }
+    
+    @objc func update() {
+        let format = DateFormatter().with {
+            $0.dateFormat = "HH:mm:ss"
+        }
+        timeLabel.text = format.string(from: Date())
+        
+        
     }
 
 
